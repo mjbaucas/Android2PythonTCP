a2d79127eb15f96df98b78244db0b58183096ffb
MBaucas <mjbaucas@gmail.com>
2021-11-09 23:29:08 -0500

    Attempt to fix pi client 4

--- app/src/server/client.py
+++ app/src/server/client.py
@@ -10,15 +10,17 @@ port = int(sys.argv[2])
 data = size_selector(sys.argv[3])
 limit = 60000
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((host_ip, port))
+
 
 timer = time.time()
 current = time.time()
 while current-timer < limit:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((host_ip, port))
+    
     start = time.time()
     s.sendall(str.encode(data))
-    recieved = s.recv(1024)
+    recieved = s.recv(2048)
     end = time.time()
     average += end - start
 
