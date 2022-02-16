8a4a89829228227e64fe929f36d68c9281d345bd
mjbaucas <mjbaucas@gmail.com>
2022-02-15 21:27:17 -0500

    Fixing socket

--- app/src/server/client.py
+++ app/src/server/client.py
@@ -19,11 +19,11 @@ while current-timer < limit:
     s.connect((host_ip, port))
     s.sendall(str.encode(data))
     recieved = recvall(s, len(data))
+    end = time.time()
+    diff = end-current
     if(len(recieved) == len(data)):
         print(len(recieved))
         print('Recieved', repr(recieved))
-        end = time.time()
-        diff = end-current
         total += diff
         counter += 1
         current = end
