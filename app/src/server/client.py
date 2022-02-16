a89ca1c5d1fdea514f1699362df2767ba13f4e3a
mjbaucas <mjbaucas@gmail.com>
2022-02-15 21:30:59 -0500

    Fixed socket

--- app/src/server/client.py
+++ app/src/server/client.py
@@ -18,7 +18,7 @@ while current-timer < limit:
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((host_ip, port))
     s.sendall(str.encode(data))
-    recieved = recvall(s, len(data))
+    recieved = s.recv(len(data))
     end = time.time()
     diff = end-current
     current = end
