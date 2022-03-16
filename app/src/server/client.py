adc7c765397580c6b415350146a0d1d686f297a3
mjbaucas <mjbaucas@gmail.com>
2022-03-16 15:43:01 -0400

    Minor fix to client code

--- app/src/server/client.py
+++ app/src/server/client.py
@@ -31,6 +31,9 @@ trusted_list = [
     "default",
 ]
 
+# Public Key generated from an Secret Key of "0"
+secret_key = "5feceb66ffc86f38d952786c6d696c79c2dbc239dd4e91b46729d73a27fb57e9"
+
 for x in range(0, size_of_block):
     trusted_list.append("item" + str(x))
 
